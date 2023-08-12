02272fb11f4a1f91b24d83ce357e015e39bf5595
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2023-08-12 14:56:12 +1000

    - Added change detection for BackUp.copy method used on individual file backup operations

--- BackUp.py
+++ BackUp.py
@@ -9,15 +9,18 @@ import re
 # Defining class and functions representing a single source / output pair
 class BackUp(Log):
     def __init__(self, source, output_targets, backup_name, limit_number, log_path='logs.txt'):
-        self.prefix = backup_name.replace("-", "")
+        self.backup_name = backup_name.replace("-", "")
         self.limit = limit_number
         self.source = source
+        self.file_modified_dt = None
+        self.folder_modified_dt = None
+        self.file_last_modified_dt(self.source)
         if type(output_targets) == str:
             self.output = [output_targets]
         elif type(output_targets) == list:
             self.output = output_targets
         else:
-            output_exception_message = f"{self.prefix} | execution halted - 'output_folders' var must be list or str"
+            output_exception_message = f"{self.backup_name} | execution halted - 'output_folders' var must be list or str"
             logging.error(output_exception_message)
             raise Exception(output_exception_message)
 
@@ -33,11 +36,13 @@ class BackUp(Log):
         super().__init__(log_path)
 
     # Get current datetime as a string
-    def get_dt_str(self):
+    @staticmethod
+    def get_dt_str():
         return datetime.today().strftime("%Y-%m-%d %H-%M %p")
 
     # Get datetime object from that string
-    def get_dt_obj(self, date_string):
+    @staticmethod
+    def get_dt_obj(date_string):
         return datetime.strptime(date_string, "%Y-%m-%d %H-%M %p")
 
     # Get a list of folders in a folder and sort them by the datetime string
@@ -46,15 +51,15 @@ class BackUp(Log):
         for entry in os.scandir(path):
             folder_path = entry.path
             folder_name = entry.name
-            if re.match(f"^{self.prefix}", folder_name):
+            if re.match(f"^{self.backup_name}", folder_name):
                 try:
                     folder_datetime = self.get_dt_obj(folder_name.split(" - ", 1)[1])
                     folders.append((folder_path, folder_name, folder_datetime))
                 except ValueError:
-                    logging.warning(f"{self.prefix} | folder '{folder_name}' in '{path}'"
+                    logging.warning(f"{self.backup_name} | folder '{folder_name}' in '{path}'"
                                     f" did not match date time format and was skipped")
             else:
-                logging.info(f"{self.prefix} | folder '{folder_name}' in '{path}'"
+                logging.info(f"{self.backup_name} | folder '{folder_name}' in '{path}'"
                              f" did not start with prefix and was skipped")
         sorted_folders = sorted(folders, key=lambda x: x[2])
         return sorted_folders
@@ -63,21 +68,21 @@ class BackUp(Log):
     # Delete the oldest folders outside of the limit property
     def copy_folder(self):
         for output_path in self.output:
-            file_name = self.prefix + " - " + self.get_dt_str()
+            file_name = self.backup_name + " - " + self.get_dt_str()
             output_folder_details = self.get_folders(output_path)
             output_subdir = os.path.join(output_path, file_name)
             if file_name not in [x[1] for x in output_folder_details]:
                 shutil.copytree(self.source, output_subdir)
-                logging.info(f"{self.prefix} | Folder | Saved '{output_subdir}'")
+                logging.info(f"{self.backup_name} | Folder | Saved '{output_subdir}'")
             else:
-                logging.info(f"{self.prefix} | Folder | Did not save '{output_subdir}' - file already exists")
+                logging.info(f"{self.backup_name} | Folder | Did not save '{output_subdir}' - file already exists")
             output_folder_details_after = self.get_folders(output_path)
             if len(output_folder_details_after) > self.limit:
                 folders_to_keep = output_folder_details_after[-abs(self.limit):]
                 for folder in output_folder_details_after:
                     if folder not in folders_to_keep:
                         shutil.rmtree(folder[0])
-                        logging.info(f"{self.prefix} | Folder | Deleted '{folder[0]}'"
+                        logging.info(f"{self.backup_name} | Folder | Deleted '{folder[0]}'"
                                      f" - old folder outside limit ({self.limit})")
 
     # Get a list of files in the source folder
@@ -88,15 +93,15 @@ class BackUp(Log):
                 file_path = os.path.join(root, file)
                 file_name = os.path.relpath(file_path, directory)
                 file_name_without_extension = os.path.splitext(file_name)[0]
-                if re.match(f"^{self.prefix}", file_name):
+                if re.match(f"^{self.backup_name}", file_name):
                     try:
                         file_datetime = self.get_dt_obj(file_name_without_extension.split(" - ", 1)[1])
                         file_details.append((file_path, file_name, file_datetime))
                     except ValueError:
-                        logging.warning(f"{self.prefix} | file '{file_name}' in '{directory}'"
+                        logging.warning(f"{self.backup_name} | file '{file_name}' in '{directory}'"
                                         f" did not match date time format and was skipped")
                 else:
-                    logging.info(f"{self.prefix} | folder '{file_name}' in '{directory}'"
+                    logging.info(f"{self.backup_name} | folder '{file_name}' in '{directory}'"
                                  f" did not start with prefix and was skipped")
         sorted_files = sorted(file_details, key=lambda x: x[2])
         return file_details
@@ -105,15 +110,15 @@ class BackUp(Log):
     # Delete the oldest files outside of the limit property
     def copy_file(self, ext):
         for output_path in self.output:
-            file_name = self.prefix + " - " + self.get_dt_str()
+            file_name = self.backup_name + " - " + self.get_dt_str()
             output_folder_details = self.get_files(output_path)
             output_file = os.path.join(output_path, file_name) + f".{ext}"
             if file_name not in [x[1] for x in output_folder_details]:
                 shutil.copy(self.source, output_file)
-                logging.info(f"{self.prefix} | File | Saved '{output_file}'")
+                logging.info(f"{self.backup_name} | File | Saved '{output_file}'")
             else:
                 logging.info(
-                    f"{self.prefix} | File | Did not save '{output_file}' - file already exists")
+                    f"{self.backup_name} | File | Did not save '{output_file}' - file already exists")
             output_folder_details_after = self.get_files(output_path)
             if len(output_folder_details_after) > self.limit:
                 files_to_keep = output_folder_details_after[-abs(self.limit):]
@@ -121,17 +126,37 @@ class BackUp(Log):
                     if file not in files_to_keep:
                         os.remove(file[0])
                         logging.info(
-                            f"{self.prefix} | File | Deleted '{file[0]}' - old file outside limit ({self.limit})")
+                            f"{self.backup_name} | File | Deleted '{file[0]}' - old file outside limit ({self.limit})")
+
+    # Gets the last modified date of a file
+    def file_last_modified_dt(self, file_path):
+        try:
+            modification_time = os.path.getmtime(file_path)
+            last_modified_date = datetime.fromtimestamp(modification_time)
+            self.file_modified_dt = last_modified_date
+        except OSError:
+            return f"Source file '{file_path}' was not found"
 
     # Run the 'copy_file' or 'copy_folder' functions based on log_path type detection of the source
     # If file, pass the file extention to the 'copy_file' argument
+    # Only undertakes the copy method if the source has changed since the last logged backup
     def copy(self):
-        if os.path.isdir(self.source):
+        is_dir = os.path.isdir(self.source)
+        is_file = os.path.isfile(self.source)
+        is_either = any([is_dir, is_file])
+        # If the source is a directory
+        if is_dir:
             self.copy_folder()
-        elif os.path.isfile(self.source):
+        # If the source is a file
+        elif is_file and self.file_modified_dt > self.filter_logs(self.backup_name, 'saves').last_log_dt:
             file_ext = self.source.split(".")[-1]
             self.copy_file(file_ext)
+        # if it was either a file or directory but failed the conditions above due to lack of changes
+        elif is_either:
+            no_changes_message = f"{self.backup_name} | File | DidNothing '{self.source}' no changes since last log"
+            logging.info(no_changes_message)
+        # Source variable had issues
         else:
-            source_exception_message = f"{self.prefix} | execution halted - 'source' var must be file or directory"
+            source_exception_message = f"{self.backup_name} | File | execution halted - 'source' var must be file or directory"
             logging.error(source_exception_message)
             raise Exception(source_exception_message)
--- Log.py
+++ Log.py
@@ -39,7 +39,7 @@ class Log:
             # Get log type
             line_dict["logtype"] = list_items[1]
 
-            # Get identifier aka prefix
+            # Get identifier aka backup_name
             line_dict["backup_name"] = list_items[2]
 
             # Get file type
@@ -47,7 +47,7 @@ class Log:
 
             # Get action that was taken per line
             line_dict["action"] = list_items[4].split("'")[0].strip()
-            accepted_actions = ('Saved', 'Deleted')
+            accepted_actions = ('Saved', 'Deleted', 'DidNothing')
             if line_dict["action"] not in accepted_actions:
                 raise Exception(f"Log file features unknown action status not in {accepted_actions}, please review")
 
@@ -73,22 +73,36 @@ class Log:
         if action not in allowed:
             raise Exception(f"Argument for self.action() must be in f{allowed}")
         # Checking backup_name input
+        exists = 'y'
         if backup_name not in [x["backup_name"] for x in self.log_data]:
-            raise Exception(f"Argument for self.backup_names() does not exist in log file")
+            exists = 'n'
+            while True:
+                user_input = input(f"Argument for self.backup_names() does not exist in log file. \n"
+                                   f"This may be the first backup or a mispelling of the backup name. \n"
+                                   f"Do you want to continue with backup '{backup_name}'? (y/n): \n").strip().lower()
+                if user_input == 'n':
+                    raise Exception(f"Rename self.backup_names() and re-run script")
+                elif user_input != 'y':
+                    print("Invalid input. Please enter 'y' or 'n'.")
+                else:
+                    print("Continuing with backup operation.")
+                    break
 
-        # Filter log_data property per backup_name
-        self.log_data = [x for x in self.original_log_data if x["backup_name"] == backup_name]
+        # Only set log values if backup name existed at outset
+        if exists == 'y':
+            # Filter log_data property per backup_name
+            self.log_data = [x for x in self.original_log_data if x["backup_name"] == backup_name]
 
-        # Filter log_data property per action
-        if action == 'all':
-            self.log_data = self.log_data
-        if action == 'saves':
-            self.log_data = [x for x in self.log_data if x["action"] == 'Saved']
-        if action == 'deletes':
-            self.log_data = [x for x in self.log_data if x["action"] == 'Deleted']
+            # Filter log_data property per action
+            if action == 'all':
+                self.log_data = self.log_data
+            if action == 'saves':
+                self.log_data = [x for x in self.log_data if x["action"] == 'Saved']
+            if action == 'deletes':
+                self.log_data = [x for x in self.log_data if x["action"] == 'Deleted']
 
-        # Update values for max properties based on the now filtered self.log_data
-        self.set_properties()
+            # Update values for max properties based on the now filtered self.log_data
+            self.set_properties()
 
         # Return self for argument chaining
         return self
