2a0c86681e99203575cc315b8f6295feac5dd3fd
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2024-08-18 13:04:39 +1000

    Added log and script examples

---
+++ examples/scripts/run_backups.bat
@@ -0,0 +1,1 @@
+poetry run python run_backups.py
---
+++ examples/scripts/run_backups.py
@@ -0,0 +1,12 @@
+from multiversion_backup.backup import BackUp
+
+# Create BackUp objects
+important_backup = BackUp(
+    r"C:\Program Files\Important Folder",  # Source
+    r"C:\Users\johnc\OneDrive\Backup Storage Folder",  # Storage
+    "my_backup",  # Prefix
+    5,  # Max versions
+)
+
+# Run backup operations
+important_backup.copy()
